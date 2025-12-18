a676c850162182df82f4cf9f00b281531a1b16fd
Kpoemegbe Gideon <kpoemegbegideon@gmail.com>
2025-12-18 21:00:53 +0000

    Order failed , an unexpected error occured, please try again later

--- src/ai/dev.ts
+++ src/ai/dev.ts
@@ -1,7 +1,4 @@
 
-import { config } from 'dotenv';
-config();
-
 import '@/ai/flows/generate-product-titles.ts';
 import '@/ai/flows/get-product-recommendations.ts';
 import '@/ai/flows/generate-product-descriptions.ts';
